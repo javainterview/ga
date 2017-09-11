b82efb7866d8015a398c414740bcb6916e2ec26d
javainterview <31838781+javainterview@users.noreply.github.com>
2017-09-11 10:29:58 +0530

    Create ga.js

---
+++ ga.js
@@ -0,0 +1,13 @@
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-65222240-3', 'auto', {'allowLinker': true});
+ga('require', 'linker');
+ga('linker:autoLink', ['phonegaplearning.blogspot.com.au'] );
+ga('send', 'pageview');
+ga('send', 'event', 'Videos', 'play', 'Fall Campaign');
+</script>
